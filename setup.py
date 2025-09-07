256835b009241296529172b3f65ca825f17e4a89
Eugene Evstafev <ee345@cam.ac.uk>
2025-09-07 14:50:56 +0100

    docs: add long_description

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+
 setup(
     name='setup_py_gen',
-    version='2025.9.81442',
+    version='2025.9.81450',
     description='A package generated from provided source code and metadata.',
-    long_description='',
+    long_description=long_description,
     author='Eugene Evstafev',
     author_email='hi@eugene.plus',
     url='https://github.com/chigwell/setup_py_gen',
